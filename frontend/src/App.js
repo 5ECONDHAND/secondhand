779267a0167f0c4c7cbb701acbf851f980558cd1
Muhammad Fajar Alfath <mfalfath25@gmail.com>
2022-06-13 23:31:56 +0700

    chore: initiate react stacks

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import './App.css'
+import '@fontsource/poppins/400.css'
+import '@fontsource/poppins/500.css'
+import '@fontsource/poppins/700.css'
+import Router from './routes/Router'
+
+function App() {
+  return (
+    <div className="App">
+      <Router />
+    </div>
+  )
+}
+
+export default App
