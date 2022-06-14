19d654d9453b67cf30e98788961110f6e2241067
Muhammad Fajar Alfath <mfalfath25@gmail.com>
2022-06-14 16:28:39 +0700

    feat: add regiter form

---
+++ frontend/src/components/molecules/login/LoginForm.jsx
@@ -0,0 +1,89 @@
+import { useState } from 'react'
+import {
+  Box,
+  Button,
+  FormControl,
+  FormHelperText,
+  IconButton,
+  InputAdornment,
+  OutlinedInput,
+} from '@mui/material'
+import { FiEye, FiEyeOff } from 'react-icons/fi'
+
+const LoginForm = () => {
+  const [showPassword, setShowPassword] = useState(false)
+  const [values, setValues] = useState({
+    email: '',
+    password: '',
+  })
+
+  const handleChange = (prop) => (event) => {
+    setValues({ ...values, [prop]: event.target.value })
+    console.log(values)
+  }
+
+  const handleClickShowPassword = () => {
+    setShowPassword(!showPassword)
+    console.log(showPassword)
+  }
+
+  const handleMouseDownPassword = (event) => {
+    event.preventDefault()
+  }
+
+  return (
+    <>
+      <Box component="form" autoComplete="off" sx={{ display: 'flex', flexDirection: 'column' }}>
+        <FormControl sx={{ minWidth: '50ch' }}>
+          <FormHelperText sx={{ fontSize: '1rem', color: 'black', m: 0 }}>Email</FormHelperText>
+          <OutlinedInput
+            placeholder="Contoh: johndee@gmail.com"
+            type="email"
+            value={values.email}
+            onChange={handleChange('email')}
+            sx={{ borderRadius: '1rem', mb: '1rem' }}
+          />
+        </FormControl>
+        <FormControl sx={{ minWidth: '50ch', mb: '2rem' }}>
+          <FormHelperText sx={{ fontSize: '1rem', color: 'black', m: 0 }}>Password</FormHelperText>
+          <OutlinedInput
+            placeholder="Masukkan password"
+            sx={{ borderRadius: '1rem' }}
+            type={showPassword ? 'text' : 'password'}
+            value={values.password}
+            onChange={handleChange('password')}
+            endAdornment={
+              <InputAdornment position="end" sx={{ mr: '0.5rem' }}>
+                <IconButton
+                  aria-label="toggle password visibility"
+                  onClick={handleClickShowPassword}
+                  onMouseDown={handleMouseDownPassword}
+                  edge="end"
+                >
+                  {values.showPassword ? <FiEyeOff /> : <FiEye />}
+                </IconButton>
+              </InputAdornment>
+            }
+          />
+        </FormControl>
+        <Button
+          type="submit"
+          fullWidth
+          variant="contained"
+          size="large"
+          disableElevation
+          sx={{
+            borderRadius: '1rem',
+            textTransform: 'none',
+            background: '#7126B5',
+            py: '15px',
+          }}
+        >
+          Masuk
+        </Button>
+      </Box>
+    </>
+  )
+}
+
+export default LoginForm
