7c547b7d0109159373ecd6aa68ef57146b35449a
ivan <ivannovaa.24@gmail.com>
2022-06-14 22:57:05 +0700

    style:add components

---
+++ frontend/src/components/atoms/home/banner/Banner.jsx
@@ -0,0 +1,34 @@
+import { Box, Typography } from '@mui/material'
+import React from 'react'
+import banner from '../../../../assets/images/banner-image.png'
+const Banner = () => {
+  return (
+    <>
+      <Box sx={{ width: '100%', display: 'flex', justifyContent: 'space-between', alignItems: 'center', marginTop: '32px', gap: '16px' }}>
+        <Box display={{ xs: 'none', md: 'block' }} sx={{ width: '236px', height: '224px', backgroundColor: '#B6D4A8', borderRadius: '0 20px 20px 0' }}></Box>
+        <Box sx={{ width: '100%', height: '288px', backgroundColor: '#FFE9CA', borderRadius: '20px', display: 'flex', alignItems: 'center', justifyContent: 'space-between', paddingLeft: { xs: '10px', sm: '10px', md: '10px', lg: '80px' } }}>
+          <Box sx={{ width: { xs: '100%', md: '100%', lg: '50%' } }}>
+            <Typography variant='h3'>Bulan Ramadhan  Banyak diskon!</Typography>
+            <Typography variant='body1'>Diskon Hingga</Typography>
+            <Typography variant='h4' color='error'>60%</Typography>
+          </Box>
+          <Box sx={{ width: '50%', display: { xs: 'none', sm: 'none', md: 'none', lg: 'flex' }, justifyContent: 'flex-end', position: 'relative' }}>
+            <img src={banner} alt='banner' width='100%' height='288px' />
+            <Box
+              sx={{
+                display: { xs: 'none', md: 'block' },
+                position: 'absolute',
+                height: '100%',
+                width: '37vw',
+                background: 'linear-gradient(to right, #FFE9CA 5%, rgba(160, 110, 206, 0) 80%)',
+              }}
+            />
+          </Box>
+        </Box>
+        <Box display={{ xs: 'none', md: 'block' }} sx={{ width: '236px', height: '224px', backgroundColor: '#E2D4F0', borderRadius: '20px 0 0 20px' }}></Box>
+      </Box>
+    </>
+  )
+}
+
+export default Banner
--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,19 +1,65 @@
 import React from 'react'
-import { Box, Container, Typography } from '@mui/material';
-import Buttons from '../../components/buttons/Buttons';
-const Home = () => {
+import { Box, Button, Container, Grid, Typography } from '@mui/material';
+import { Banner, Buttons, CardItem } from '../../components/atoms/home';
+import { FiPlus } from 'react-icons/fi';
 
+
+const Home = () => {
 
   return (
     <>
-      <Box sx={{ width: '100%', backgroundColor: 'red', height: '200px' }}>
-
-      </Box>
-      <Container maxWidth='xl' sx={{ marginTop: '40px' }}>
+      <Banner />
+      <Container maxWidth='xl' sx={{ marginY: '40px', position: 'relative' }}>
         <Typography variant='h6' sx={{ fontSize: '16px' }}>
           Telusuri Kategori
         </Typography>
         <Buttons />
+        <Grid container columns={{ xs: 1, sm: 2, md: 4, lg: 6 }} mt={{ xs: '20px', md: '30px' }} spacing={2}>
+          <Grid item xs={1}>
+            <CardItem />
+          </Grid>
+          <Grid item xs={1}>
+            <CardItem />
+          </Grid>
+          <Grid item xs={1}>
+            <CardItem />
+          </Grid>
+          <Grid item xs={1}>
+            <CardItem />
+          </Grid>
+          <Grid item xs={1}>
+            <CardItem />
+          </Grid>
+          <Grid item xs={1}>
+            <CardItem />
+          </Grid>
+          <Grid item xs={1}>
+            <CardItem />
+          </Grid>
+          <Grid item xs={1}>
+            <CardItem />
+          </Grid>
+        </Grid>
+        <Button sx={{
+          backgroundColor: '#7126B5',
+          color: 'white',
+          position: 'fixed',
+          bottom: '50px',
+          left: '0',
+          right: '0',
+          margin: 'auto',
+          zIndex: '10',
+          borderRadius: '12px',
+          padding: '8px 15px',
+          ':hover': {
+            bgcolor: '#631fa1',
+            color: 'white'
+          },
+        }}
+          startIcon={<FiPlus />}
+        >
+          Jual
+        </Button>
       </Container>
 
     </>
