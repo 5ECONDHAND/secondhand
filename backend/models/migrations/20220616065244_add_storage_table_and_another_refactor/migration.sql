74b388e231222893e329bb98ff44630995519292
BroNils <nonstop.hacking.free@gmail.com>
2022-06-16 14:00:39 +0700

    Bunch of refactor in prisma schema
    refactor:
    - some relation
    - some column type
    - some name

---
+++ backend/models/migrations/20220616065244_add_storage_table_and_another_refactor/migration.sql
@@ -0,0 +1,68 @@
+-- AlterTable
+ALTER TABLE "Product" ALTER COLUMN "price" SET DEFAULT 0;
+
+-- AlterTable
+ALTER TABLE "Storage" ALTER COLUMN "size" SET DEFAULT 0;
+
+-- CreateTable
+CREATE TABLE "TransactionsOnUsers" (
+    "offeredPrice" DECIMAL(12,2) NOT NULL,
+    "description" TEXT,
+    "accepted" BOOLEAN NOT NULL DEFAULT false,
+    "createdAt" TIMESTAMPTZ(6) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "userId" BIGINT NOT NULL,
+    "transactionId" BIGINT NOT NULL,
+
+    CONSTRAINT "TransactionsOnUsers_pkey" PRIMARY KEY ("userId","transactionId")
+);
+
+-- CreateTable
+CREATE TABLE "CategoriesOnProducts" (
+    "createdAt" TIMESTAMPTZ(6) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "categoryId" BIGINT NOT NULL,
+    "productId" BIGINT NOT NULL,
+
+    CONSTRAINT "CategoriesOnProducts_pkey" PRIMARY KEY ("categoryId","productId")
+);
+
+-- CreateTable
+CREATE TABLE "ProductsOnStorages" (
+    "createdAt" TIMESTAMPTZ(6) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "productId" BIGINT NOT NULL,
+    "storageId" BIGINT NOT NULL,
+
+    CONSTRAINT "ProductsOnStorages_pkey" PRIMARY KEY ("storageId","productId")
+);
+
+-- CreateTable
+CREATE TABLE "StoragesOnUsers" (
+    "createdAt" TIMESTAMPTZ(6) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "storageId" BIGINT NOT NULL,
+    "userId" BIGINT NOT NULL,
+
+    CONSTRAINT "StoragesOnUsers_pkey" PRIMARY KEY ("storageId","userId")
+);
+
+-- AddForeignKey
+ALTER TABLE "TransactionsOnUsers" ADD CONSTRAINT "TransactionsOnUsers_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "TransactionsOnUsers" ADD CONSTRAINT "TransactionsOnUsers_transactionId_fkey" FOREIGN KEY ("transactionId") REFERENCES "Transaction"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "CategoriesOnProducts" ADD CONSTRAINT "CategoriesOnProducts_productId_fkey" FOREIGN KEY ("productId") REFERENCES "Product"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "CategoriesOnProducts" ADD CONSTRAINT "CategoriesOnProducts_categoryId_fkey" FOREIGN KEY ("categoryId") REFERENCES "Category"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "ProductsOnStorages" ADD CONSTRAINT "ProductsOnStorages_productId_fkey" FOREIGN KEY ("productId") REFERENCES "Product"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "ProductsOnStorages" ADD CONSTRAINT "ProductsOnStorages_storageId_fkey" FOREIGN KEY ("storageId") REFERENCES "Storage"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "StoragesOnUsers" ADD CONSTRAINT "StoragesOnUsers_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "StoragesOnUsers" ADD CONSTRAINT "StoragesOnUsers_storageId_fkey" FOREIGN KEY ("storageId") REFERENCES "Storage"("id") ON DELETE CASCADE ON UPDATE CASCADE;
