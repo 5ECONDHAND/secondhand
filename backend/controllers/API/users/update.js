9f2ce1851e17e66dfd250b238b2f5e72bc1d22c4
BroNils <nonstop.hacking.free@gmail.com>
2022-06-17 17:00:58 +0700

    feat: add CRUD User
    *this only adds CRUD without authorization

---
+++ backend/controllers/API/users/update.js
@@ -0,0 +1,62 @@
+const crypto = require('crypto');
+/** @type {import('../../../models/instance')} */
+const prisma = require(process.env.ROOT_PATH + '/models/instance');
+
+/**
+ *
+ * @param {import('express').Request} req
+ * @param {import('express').Response} res
+ * @param {import('express').NextFunction} next
+ */
+async function controller(req, res, next) {
+  var id = parseInt(req.params.id);
+
+  if(isNaN(id)) {
+    return res.json({
+      error: true,
+      message: 'Invalid user id',
+      data: [],
+    })
+  }
+
+  const hash = req.body.password
+  ? crypto.createHash('sha512').update(req.body.password).digest('hex')
+  : null;
+
+  var dataPayload = {
+    email: req.body.email,
+    fullname: req.body.fullname,
+    password: hash,
+    phoneNo: req.body.phoneNo,
+    city: req.body.city,
+    address: req.body.address,
+    updatedAt: new Date()
+  };
+  //remove key with null value
+  dataPayload = Object.fromEntries(Object.entries(dataPayload).filter(([_, v]) => v != null));
+
+  const data = await prisma.user.update({
+    where: {
+      id
+    },
+    data: dataPayload
+  }).catch(err => {
+    return {
+      error: true,
+      message: err.message,
+      data: [],
+    }
+  });
+
+  if(data && data.error){
+    return res.json(data)
+  }
+
+  res.json({
+    error: false,
+    message: 'User updated',
+    data: [data],
+  })
+}
+
+module.exports = controller;
--- backend/routes/api.js
+++ backend/routes/api.js
@@ -7,6 +7,14 @@ var rootController = path.join(process.env.ROOT_PATH, '/controllers/API');
  * Load API controllers
  */
 var nfHandler = require(path.join(rootController, '/nf'));
+var userCRUD = require(path.join(rootController, 'users'));
+
+router.get('/users', userCRUD.all);
+router.get('/users/:id', userCRUD.view);
+router.post('/users', userCRUD.store);
+router.put('/users/:id', userCRUD.update);
+router.patch('/users/:id', userCRUD.update);
+router.delete('/users/:id', userCRUD.destroy);
 
 // catch 404 and respond json
 router.all('/*', nfHandler);
