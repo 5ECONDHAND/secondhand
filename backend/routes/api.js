4ca45e9aebdff4b0e84c2e7bc67406198bb2f796
BroNils <nonstop.hacking.free@gmail.com>
2022-06-17 20:32:47 +0700

    feat: add CRUD Notification

--- backend/routes/api.js
+++ backend/routes/api.js
@@ -8,6 +8,7 @@ var rootController = path.join(process.env.ROOT_PATH, '/controllers/API');
  */
 var nfHandler = require(path.join(rootController, '/nf'));
 var userCRUD = require(path.join(rootController, 'users'));
+var notificationCRUD = require(path.join(rootController, 'notifications'));
 
 router.get('/users', userCRUD.all);
 router.get('/users/:id', userCRUD.view);
@@ -15,6 +16,13 @@ router.post('/users', userCRUD.store);
 router.put('/users/:id', userCRUD.update);
 router.patch('/users/:id', userCRUD.update);
 router.delete('/users/:id', userCRUD.destroy);
+
+router.get('/notifications', notificationCRUD.all);
+router.get('/notifications/:id', notificationCRUD.view);
+router.post('/notifications', notificationCRUD.store);
+router.put('/notifications/:id', notificationCRUD.update);
+router.patch('/notifications/:id', notificationCRUD.update);
+router.delete('/notifications/:id', notificationCRUD.destroy);
 
 // catch 404 and respond json
 router.all('/*', nfHandler);
