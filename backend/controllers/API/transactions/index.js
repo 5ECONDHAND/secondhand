5765b121ac6247b8969c7121b12b9fe4374181a1
BroNils <nonstop.hacking.free@gmail.com>
2022-06-24 15:26:05 +0700

    fomatting or beautify some codes

--- backend/controllers/API/transactions/index.js
+++ backend/controllers/API/transactions/index.js
@@ -1,7 +1,7 @@
 module.exports = {
-    store: require('./store'),
-    update: require('./update'),
-    destroy: require('./destroy'),
-    view: require('./view'),
-    all: require('./all'),
-  }
+  store: require("./store"),
+  update: require("./update"),
+  destroy: require("./destroy"),
+  view: require("./view"),
+  all: require("./all"),
+};
