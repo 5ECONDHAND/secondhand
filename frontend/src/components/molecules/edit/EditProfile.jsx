0e75233ee70bb945a6abec0ea39100ad892e1f8f
Taufiq <iro.yakata@gmail.com>
2022-06-29 21:11:16 +0700

    feat: bind edit profile

--- frontend/src/components/molecules/edit/EditProfile.jsx
+++ frontend/src/components/molecules/edit/EditProfile.jsx
@@ -1,54 +1,69 @@
-import { useEffect, useState } from 'react'
-import { FormControl, FormHelperText, OutlinedInput, Button, Box, Grid } from '@mui/material'
-import gambar from '../../../assets/images/Profile.png'
-import { validateProfile } from '../../../utils/validators'
+import { useState } from "react";
+import {
+  FormControl,
+  FormHelperText,
+  OutlinedInput,
+  Button,
+  Box,
+  Grid,
+} from "@mui/material";
+import gambar from "../../../assets/images/Profile.png";
+import { validateProfile } from "../../../utils/validators";
 
-import { useEditUserMutation } from '../../../redux/services/editApi'
-import { useDispatch } from 'react-redux'
-import { useNavigate } from 'react-router-dom'
+import { useEditUserMutation } from "../../../redux/services/editApi";
+import { useParams } from "react-router-dom";
+
 const EditProfile = () => {
-  const navigate = useNavigate()
-  const dispatch = useDispatch()
-  const [preview, setPreview] = useState(null)
-  const [error, setError] = useState({})
+  const [preview, setPreview] = useState(null);
+  
+  const [error, setError] = useState({});
   const [values, setValues] = useState({
-    nama: '',
-    kota: '',
-    alamat: '',
-    nomor: '',
-  })
-  const [editUser] = useEditUserMutation()
+    nama: "",
+    kota: "",
+    alamat: "",
+    nomor: "",
+  });
+  const jwtToken = JSON.parse(localStorage.getItem("user")).token;
+  const [editUser] = useEditUserMutation();
+  const { userId } = useParams()
 
   const handleSubmit = async (event) => {
-    event.preventDefault()
-    validateProfile(values, setError)
-    await editUser({ fullname : values.nama, city: values.kota, address: values.alamat, phoneNo: values.nomor })
-    console.log(values)
-    console.log(error)
-  }
+    event.preventDefault();
+    validateProfile(values, setError);
+    await editUser({
+      id: userId,
+      fullname: values.nama,
+      city: values.kota,
+      address: values.alamat,
+      phoneNo: values.nomor,
+      token: jwtToken,
+    });
+    console.log(values);
+    console.log(error);
+  };
 
   const handleChange = (prop) => (event) => {
-    setValues({ ...values, [prop]: event.target.value })
-  }
+    setValues({ ...values, [prop]: event.target.value });
+  };
 
   const handleImage = (e) => {
-    const selected = e.target.files[0]
-    const types = ['image/png', 'image/jpeg', 'image/jpg']
+    const selected = e.target.files[0];
+    const types = ["image/png", "image/jpeg", "image/jpg"];
     if (selected && types.includes(selected.type)) {
-      let reader = new FileReader()
+      let reader = new FileReader();
       reader.onloadend = () => {
-        setPreview(reader.result)
-      }
-      reader.readAsDataURL(selected)
+        setPreview(reader.result);
+      };
+      reader.readAsDataURL(selected);
     }
-  }
+  };
 
   return (
     <div>
       <Box component="form" autoComplete="off" onSubmit={handleSubmit}>
         <Grid container direction="column">
           <Grid item>
-            <Box sx={{ display: 'flex', justifyContent: 'center', mb: 3 }}>
+            <Box sx={{ display: "flex", justifyContent: "center", mb: 3 }}>
               {!preview && (
                 <Button
                   variant="contained"
@@ -56,12 +71,12 @@ const EditProfile = () => {
                   onChange={handleImage}
                   disableElevation
                   sx={{
-                    background: '#E2D4F0',
-                    border: 'none',
-                    borderRadius: '12px',
-                    ':hover': { background: '#E2D4F0' },
-                    width: '96px',
-                    height: '96px',
+                    background: "#E2D4F0",
+                    border: "none",
+                    borderRadius: "12px",
+                    ":hover": { background: "#E2D4F0" },
+                    width: "96px",
+                    height: "96px",
                   }}
                 >
                   <img src={gambar} alt="" />
@@ -77,74 +92,84 @@ const EditProfile = () => {
                   onChange={handleImage}
                   disableElevation
                   sx={{
-                    background: 'transparent',
-                    border: 'none',
-                    ':hover': { background: '#ebebeb' },
+                    background: "transparent",
+                    border: "none",
+                    ":hover": { background: "#ebebeb" },
                   }}
                 >
                   <img
                     src={preview}
                     alt=""
                     style={{
-                      width: '96px',
-                      height: '96px',
+                      width: "96px",
+                      height: "96px",
                     }}
                   />
                   <input type="file" hidden />
                 </Button>
               </>
             ) : (
-              ''
+              ""
             )}
           </Grid>
           <Grid item>
-            <FormControl sx={{ minWidth: { xs: '30ch', sm: '50ch' } }}>
-              <FormHelperText sx={{ fontSize: '1rem', color: 'black', m: 0 }}>Nama*</FormHelperText>
+            <FormControl sx={{ minWidth: { xs: "30ch", sm: "50ch" } }}>
+              <FormHelperText sx={{ fontSize: "1rem", color: "black", m: 0 }}>
+                Nama*
+              </FormHelperText>
               <OutlinedInput
                 error={error.nama ? true : false}
                 placeholder="Nama"
                 type="text"
                 value={values.nama}
-                onChange={handleChange('nama')}
-                sx={{ borderRadius: '1rem' }}
+                onChange={handleChange("nama")}
+                sx={{ borderRadius: "1rem" }}
               />
-              <FormHelperText sx={{ m: 0, mb: '1rem' }}>{error.nama}</FormHelperText>
+              <FormHelperText sx={{ m: 0, mb: "1rem" }}>
+                {error.nama}
+              </FormHelperText>
             </FormControl>
           </Grid>
           <Grid item>
-            <FormControl sx={{ minWidth: { xs: '30ch', sm: '50ch' } }}>
-              <FormHelperText sx={{ fontSize: '1rem', color: 'black', m: 0 }}>Kota*</FormHelperText>
+            <FormControl sx={{ minWidth: { xs: "30ch", sm: "50ch" } }}>
+              <FormHelperText sx={{ fontSize: "1rem", color: "black", m: 0 }}>
+                Kota*
+              </FormHelperText>
               <OutlinedInput
                 error={error.kota ? true : false}
                 placeholder="Isi Kota"
                 type="text"
                 value={values.kota}
-                onChange={handleChange('kota')}
-                sx={{ borderRadius: '1rem' }}
+                onChange={handleChange("kota")}
+                sx={{ borderRadius: "1rem" }}
               />
-              <FormHelperText sx={{ m: 0, mb: '1rem' }}>{error.kota}</FormHelperText>
+              <FormHelperText sx={{ m: 0, mb: "1rem" }}>
+                {error.kota}
+              </FormHelperText>
             </FormControl>
           </Grid>
           <Grid item>
-            <FormControl sx={{ minWidth: { xs: '30ch', sm: '50ch' } }}>
-              <FormHelperText sx={{ fontSize: '1rem', color: 'black', m: 0 }}>
+            <FormControl sx={{ minWidth: { xs: "30ch", sm: "50ch" } }}>
+              <FormHelperText sx={{ fontSize: "1rem", color: "black", m: 0 }}>
                 Alamat*
               </FormHelperText>
               <OutlinedInput
                 error={error.alamat ? true : false}
                 type="text"
                 placeholder="Contoh: Jalan Ikan Hiu 33"
-                onChange={handleChange('alamat')}
-                sx={{ borderRadius: '1rem' }}
+                onChange={handleChange("alamat")}
+                sx={{ borderRadius: "1rem" }}
                 multiline
                 rows={4}
               />
-              <FormHelperText sx={{ m: 0, mb: '1rem' }}>{error.alamat}</FormHelperText>
+              <FormHelperText sx={{ m: 0, mb: "1rem" }}>
+                {error.alamat}
+              </FormHelperText>
             </FormControl>
           </Grid>
           <Grid item>
-            <FormControl sx={{ minWidth: { xs: '30ch', sm: '50ch' } }}>
-              <FormHelperText sx={{ fontSize: '1rem', color: 'black', m: 0 }}>
+            <FormControl sx={{ minWidth: { xs: "30ch", sm: "50ch" } }}>
+              <FormHelperText sx={{ fontSize: "1rem", color: "black", m: 0 }}>
                 No Handphone*
               </FormHelperText>
               <OutlinedInput
@@ -152,14 +177,16 @@ const EditProfile = () => {
                 type="number"
                 placeholder="contoh: +628123456789"
                 value={values.nomor}
-                onChange={handleChange('nomor')}
-                sx={{ borderRadius: '1rem' }}
+                onChange={handleChange("nomor")}
+                sx={{ borderRadius: "1rem" }}
               />
-              <FormHelperText sx={{ m: 0, mb: '1rem' }}>{error.nomor}</FormHelperText>
+              <FormHelperText sx={{ m: 0, mb: "1rem" }}>
+                {error.nomor}
+              </FormHelperText>
             </FormControl>
           </Grid>
           <Grid item>
-            <FormControl sx={{ minWidth: { xs: '30ch', sm: '50ch' } }}>
+            <FormControl sx={{ minWidth: { xs: "30ch", sm: "50ch" } }}>
               <Button
                 type="submit"
                 fullWidth
@@ -167,10 +194,10 @@ const EditProfile = () => {
                 size="large"
                 disableElevation
                 sx={{
-                  borderRadius: '1rem',
-                  textTransform: 'none',
-                  background: '#7126B5',
-                  py: '15px',
+                  borderRadius: "1rem",
+                  textTransform: "none",
+                  background: "#7126B5",
+                  py: "15px",
                 }}
               >
                 Simpan
@@ -180,7 +207,7 @@ const EditProfile = () => {
         </Grid>
       </Box>
     </div>
-  )
-}
+  );
+};
 
-export default EditProfile
+export default EditProfile;
