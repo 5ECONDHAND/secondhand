4d429312262c1e44d46e066a048aa8b6c44934c3
Trivitaan <trivitaannisautami@gmail.com>
2022-07-01 13:50:23 +0700

    Pagination and Sorting

--- backend/controllers/API/products/all.js
+++ backend/controllers/API/products/all.js
@@ -1,5 +1,5 @@
 /** @type {import('../../../models/instance')} */
-const prisma = require(process.env.ROOT_PATH + '/models/instance');
+const prisma = require(process.env.ROOT_PATH + "/models/instance");
 
 /**
  *
@@ -7,31 +7,46 @@ const prisma = require(process.env.ROOT_PATH + '/models/instance');
  * @param {import('express').Response} res
  * @param {import('express').NextFunction} next
  */
-
-async function controller(req, res, next){
-    const data = await prisma.product.findMany({
-        include:{
-            User: true,
-            Photos: true,
-            Categories: true,
+async function controller(req, res, next) {
+  const data = await prisma.product.findMany({
+    take: req.query.take,
+    orderBy:{
+        name: req.query.orderBy
+    },
+    include: {
+      User: {
+        select: {
+          id: true,
+          phoneNo: true,
+          fullname: true,
+          email: true,
+          Photos: true,
+          createdAt: true,
+          updatedAt: true
         }
-    }).catch(err =>{
-        return{
-            error: true,
-            message: err.message,
-            data: [],
-        }
-    });
+      },
+      Photos: true,
+      Categories: true,
+      Transaction: true
+    },
+  }).catch((err) => {
+    return {
+      error: true,
+      message: err.message,
+      data: [],
+    };
+  });
 
-    if(data && data.error){
-        return res.json(data)
-    }
+  if (data && data.error) {
+    return res.json(data);
+  }
 
-    res.json({
-        error: false,
-        message: "Fetching Data Successfully",
-        data,
-    });
+  res.json({
+    error: false,
+    message: "Success",
+    data,
+  });
+
 }
 
 module.exports = controller;
