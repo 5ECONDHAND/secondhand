728c4946a0378eb731e5aec5efd4d7f35f2f7743
Taufiq <iro.yakata@gmail.com>
2022-07-19 21:42:40 +0700

    fix: edit profile picture preview

--- frontend/src/components/molecules/edit/EditProfileForm.jsx
+++ frontend/src/components/molecules/edit/EditProfileForm.jsx
@@ -1,60 +1,73 @@
-import { useState, useEffect } from 'react'
-import { useDropzone } from 'react-dropzone'
-import { FormControl, FormHelperText, OutlinedInput, Alert, Button, Box, Grid } from '@mui/material'
-import { validateProfile } from '../../../utils/validators'
-import gambar from '../../../assets/images/Profile.png'
-import { useDispatch } from 'react-redux'
-import { useSnackbar } from 'notistack'
-import { useNavigate, useParams } from 'react-router-dom'
-import { userActions } from '../../../redux/slices/userSlice'
-import { useSelector } from 'react-redux'
-import { selectAuth } from '../../../redux/slices/authSlice'
-import { selectUser } from '../../../redux/slices/userSlice'
+import { useState, useEffect } from "react";
+import { useDropzone } from "react-dropzone";
+import {
+  FormControl,
+  FormHelperText,
+  OutlinedInput,
+  Alert,
+  Button,
+  Box,
+  Grid,
+} from "@mui/material";
+import { validateProfile } from "../../../utils/validators";
+import gambar from "../../../assets/images/Profile.png";
+import { useDispatch } from "react-redux";
+import { useSnackbar } from "notistack";
+import { useNavigate } from "react-router-dom";
+import { userActions } from "../../../redux/slices/userSlice";
+import { useSelector } from "react-redux";
+import { selectAuth } from "../../../redux/slices/authSlice";
+import { selectUser } from "../../../redux/slices/userSlice";
 
-import axios from 'axios'
+import axios from "axios";
+import { useGetUserSellerQuery } from "../../../redux/services/userApi";
 
 const thumb = {
-  display: 'inline-flex',
+  display: "inline-flex",
   borderRadius: 2,
-  border: '1px solid #eaeaea',
+  border: "1px solid #eaeaea",
   marginBottom: 10,
   marginTop: 10,
   width: 96,
   height: 96,
   padding: 4,
-  boxSizing: 'border-box',
-}
+  boxSizing: "border-box",
+};
 
 const thumbInner = {
-  display: 'flex',
+  display: "flex",
   minWidth: 0,
-  overflow: 'hidden',
-}
+  overflow: "hidden",
+};
 
 const img = {
-  display: 'block',
-  width: 'auto',
-  height: '100%',
-}
+  display: "block",
+  width: "auto",
+  height: "100%",
+};
 
 const EditProfileForm = () => {
-  const { enqueueSnackbar } = useSnackbar()
-  const navigate = useNavigate()
-  const dispatch = useDispatch()
-  const [error, setError] = useState({})
-  const [files, setFiles] = useState([])
+  const { enqueueSnackbar } = useSnackbar();
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const [error, setError] = useState({});
+  const [files, setFiles] = useState([]);
   const [values, setValues] = useState({
-    nama: '',
-    kota: '',
-    alamat: '',
-    nomor: '',
-  })
-  const { userId } = useParams()
-  const user = useSelector(selectAuth)
-  const userActive = useSelector(selectUser)
+    nama: "",
+    kota: "",
+    alamat: "",
+    nomor: "",
+  });
+  const user = useSelector(selectAuth);
+  const userActive = useSelector(selectUser);
+  const { data: userData, isSuccess: isUserSuccess} = useGetUserSellerQuery(
+    userActive.accessToken,
+    { refetchOnMountOrArgChange: true }
+  )
+  const image_storage_url = userData !== undefined ? `https://febesh5-dev.herokuapp.com/api/storages/${userData?.data[0].Photos[0]?.storageId}/preview` : ''
 
   const handleSubmit = async (event) => {
-    event.preventDefault()
+    event.preventDefault();
     if (validateProfile(values, setError)) {
       axios
         .put(
@@ -69,22 +82,28 @@ const EditProfileForm = () => {
           {
             headers: {
               Authorization: `Bearer ${user.token}`,
-              'Content-Type': 'multipart/form-data',
+              "Content-Type": "multipart/form-data",
             },
           }
         )
         .then(function (response) {
-          enqueueSnackbar('Profile updated', { variant: 'success', autoHideDuration: 1000 })
-          dispatch(userActions.setUserActive(response.data.data[0]))
-          console.log(response.data.data[0])
+          enqueueSnackbar("Profile updated", {
+            variant: "success",
+            autoHideDuration: 1000,
+          });
+          dispatch(userActions.setUserActive(response.data.data[0]));
+          console.log(response.data.data[0]);
           setTimeout(() => {
-            navigate('/sales')
-          }, 2000)
+            navigate("/sales");
+          }, 2000);
         })
         .catch((e) => {
-          console.log(e)
-          enqueueSnackbar('Error occurred', { variant: 'error', autoHideDuration: 1000 })
-        })
+          console.log(e);
+          enqueueSnackbar("Error occurred", {
+            variant: "error",
+            autoHideDuration: 1000,
+          });
+        });
 
       // const editData = new FormData()
       // editData.append("files", files[0])
@@ -104,16 +123,17 @@ const EditProfileForm = () => {
       //   editData
       // })
     }
-  }
+  };
 
   const handleChange = (prop) => (event) => {
-    setValues({ ...values, [prop]: event.target.value })
-  }
+    setValues({ ...values, [prop]: event.target.value });
+  };
 
   const { fileRejections, getRootProps, getInputProps } = useDropzone({
     maxFiles: 1,
+    maxSize: 5242880,
     accept: {
-      'image/*': [],
+      "image/*": [],
     },
     onDrop: (acceptedFiles) => {
       setFiles(
@@ -122,14 +142,10 @@ const EditProfileForm = () => {
             preview: URL.createObjectURL(file),
           })
         )
-      )
-      console.log(acceptedFiles)
+      );
+      console.log(acceptedFiles);
     },
-  })
-
-  const fileRejectionItems = fileRejections.map(() => {
-    return <div></div>
-  })
+  });
 
   const thumbs = files.map((file) => (
     <div style={thumb} key={file.name}>
@@ -139,108 +155,134 @@ const EditProfileForm = () => {
           style={img}
           // Revoke data uri after image is loaded
           onLoad={() => {
-            URL.revokeObjectURL(file.preview)
+            URL.revokeObjectURL(file.preview);
           }}
           alt=""
         />
       </div>
     </div>
-  ))
+  ));
 
   useEffect(() => {
     if (userActive?.city === null) {
-      enqueueSnackbar('You must first complete your profile', {
-        variant: 'warning',
+      enqueueSnackbar("You must first complete your profile", {
+        variant: "warning",
         autoHideDuration: 3000,
         preventDuplicate: true,
-      })
+      });
     }
     setValues({
-      nama: userActive.fullname ? userActive.fullname : '',
-      kota: userActive.city ? userActive.city : '',
-      alamat: userActive.address ? userActive.address : '',
-      nomor: userActive.phoneNo ? userActive.phoneNo : '',
-    })
+      nama: userActive.fullname ? userActive.fullname : "",
+      kota: userActive.city ? userActive.city : "",
+      alamat: userActive.address ? userActive.address : "",
+      nomor: userActive.phoneNo ? userActive.phoneNo : "",
+    });
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [])
+  }, []);
 
   useEffect(() => {
     // Make sure to revoke the data uris to avoid memory leaks, will run on unmount
-    return () => files.forEach((file) => URL.revokeObjectURL(file.preview))
-  }, [files])
-
+    return () => files.forEach((file) => URL.revokeObjectURL(file.preview));
+  }, [files]);
+console.log(userData);
   return (
     <div>
       <Box component="form" autoComplete="off" onSubmit={handleSubmit}>
         <Grid container direction="column">
           <Grid item>
-            <FormControl sx={{ minWidth: { xs: '30ch', sm: '50ch' } }}>
-              <FormHelperText sx={{ fontSize: '1rem', color: 'black', m: 0 }}>
-                Foto Produk
-              </FormHelperText>
-              <Box
-                {...getRootProps()}
+            {files.length === 0 ? (
+              <FormControl
                 sx={{
-                  mb: '1rem',
-                  maxWidth: { xs: '9ch', md: '9ch', lg: '9ch' },
-                  cursor: 'pointer',
+                  minWidth: { xs: "30ch", sm: "50ch" },
+                  alignItems: "center",
                 }}
               >
-                <input {...getInputProps()} />
-                {files.length !== 0 ? (
+                <Box
+                  {...getRootProps()}
+                  sx={{
+                    mb: "1rem",
+                    maxWidth: { xs: "9ch", md: "9ch", lg: "9ch" },
+                    cursor: "pointer",
+                  }}
+                >
+                  <input {...getInputProps()} />
+                  <img src={userData?.data[0].Photos[0]?.storageId ? image_storage_url : gambar} alt="" />
+                </Box>
+              </FormControl>
+            ) : (
+              <FormControl
+                sx={{
+                  minWidth: { xs: "30ch", sm: "50ch" },
+                }}
+              >
+                <Box
+                  {...getRootProps()}
+                  sx={{
+                    mb: "1rem",
+                    maxWidth: { xs: "9ch", md: "9ch", lg: "9ch" },
+                    cursor: "pointer",
+                  }}
+                >
+                  <input {...getInputProps()} />
                   <Box
                     sx={{
-                      border: '1px dashed #D0D0D0',
-                      display: 'flex',
-                      alignItems: 'center',
-                      justifyContent: 'space-around',
-                      minWidth: { xs: '30ch', md: '40ch', lg: '50ch' },
+                      border: "1px dashed #D0D0D0",
+                      display: "flex",
+                      alignItems: "center",
+                      justifyContent: "space-around",
+                      minWidth: { xs: "30ch", md: "40ch", lg: "50ch" },
                     }}
                   >
                     {thumbs}
                   </Box>
-                ) : (
-                  <img src={gambar} alt="" />
-                )}
-              </Box>
-              {fileRejectionItems[0] && (
-                <Box sx={{ mb: '1rem' }}>
-                  <Alert severity="error">Maksimal 1 Gambar dan Ukuran 5MB</Alert>
                 </Box>
-              )}
-            </FormControl>
+              </FormControl>
+            )}
+            {fileRejections.length > 0 && (
+              <Box sx={{ mb: "1rem" }}>
+                <Alert severity="error">Maksimal 1 Gambar dan Ukuran 5MB</Alert>
+              </Box>
+            )}
           </Grid>
           <Grid item>
-            <FormControl sx={{ minWidth: { xs: '30ch', sm: '50ch' } }}>
-              <FormHelperText sx={{ fontSize: '1rem', color: 'black', m: 0 }}>Nama*</FormHelperText>
+            <FormControl sx={{ minWidth: { xs: "30ch", sm: "50ch" } }}>
+              <FormHelperText sx={{ fontSize: "1rem", color: "black", m: 0 }}>
+                Nama*
+              </FormHelperText>
               <OutlinedInput
                 error={error.nama ? true : false}
                 placeholder="Nama"
                 type="text"
                 value={values.nama}
-                onChange={handleChange('nama')}
-                sx={{ borderRadius: '1rem' }}
+                onChange={handleChange("nama")}
+                sx={{ borderRadius: "1rem" }}
               />
-              <FormHelperText sx={{ m: 0, mb: '1rem' }}>{error.nama}</FormHelperText>
+              <FormHelperText sx={{ m: 0, mb: "1rem" }}>
+                {error.nama}
+              </FormHelperText>
             </FormControl>
           </Grid>
           <Grid item>
-            <FormControl sx={{ minWidth: { xs: '30ch', sm: '50ch' } }}>
-              <FormHelperText sx={{ fontSize: '1rem', color: 'black', m: 0 }}>Kota*</FormHelperText>
+            <FormControl sx={{ minWidth: { xs: "30ch", sm: "50ch" } }}>
+              <FormHelperText sx={{ fontSize: "1rem", color: "black", m: 0 }}>
+                Kota*
+              </FormHelperText>
               <OutlinedInput
                 error={error.kota ? true : false}
                 placeholder="Isi Kota"
                 type="text"
                 value={values.kota}
-                onChange={handleChange('kota')}
-                sx={{ borderRadius: '1rem' }}
+                onChange={handleChange("kota")}
+                sx={{ borderRadius: "1rem" }}
               />
-              <FormHelperText sx={{ m: 0, mb: '1rem' }}>{error.kota}</FormHelperText>
+              <FormHelperText sx={{ m: 0, mb: "1rem" }}>
+                {error.kota}
+              </FormHelperText>
             </FormControl>
           </Grid>
           <Grid item>
-            <FormControl sx={{ minWidth: { xs: '30ch', sm: '50ch' } }}>
-              <FormHelperText sx={{ fontSize: '1rem', color: 'black', m: 0 }}>
+            <FormControl sx={{ minWidth: { xs: "30ch", sm: "50ch" } }}>
+              <FormHelperText sx={{ fontSize: "1rem", color: "black", m: 0 }}>
                 Alamat*
               </FormHelperText>
               <OutlinedInput
@@ -248,17 +290,19 @@ const EditProfileForm = () => {
                 type="text"
                 placeholder="Contoh: Jalan Ikan Hiu 33"
                 value={values.alamat}
-                onChange={handleChange('alamat')}
-                sx={{ borderRadius: '1rem' }}
+                onChange={handleChange("alamat")}
+                sx={{ borderRadius: "1rem" }}
                 multiline
                 rows={4}
               />
-              <FormHelperText sx={{ m: 0, mb: '1rem' }}>{error.alamat}</FormHelperText>
+              <FormHelperText sx={{ m: 0, mb: "1rem" }}>
+                {error.alamat}
+              </FormHelperText>
             </FormControl>
           </Grid>
           <Grid item>
-            <FormControl sx={{ minWidth: { xs: '30ch', sm: '50ch' } }}>
-              <FormHelperText sx={{ fontSize: '1rem', color: 'black', m: 0 }}>
+            <FormControl sx={{ minWidth: { xs: "30ch", sm: "50ch" } }}>
+              <FormHelperText sx={{ fontSize: "1rem", color: "black", m: 0 }}>
                 No Handphone*
               </FormHelperText>
               <OutlinedInput
@@ -266,14 +310,16 @@ const EditProfileForm = () => {
                 type="number"
                 placeholder="contoh: +628123456789"
                 value={values.nomor}
-                onChange={handleChange('nomor')}
-                sx={{ borderRadius: '1rem' }}
+                onChange={handleChange("nomor")}
+                sx={{ borderRadius: "1rem" }}
               />
-              <FormHelperText sx={{ m: 0, mb: '1rem' }}>{error.nomor}</FormHelperText>
+              <FormHelperText sx={{ m: 0, mb: "1rem" }}>
+                {error.nomor}
+              </FormHelperText>
             </FormControl>
           </Grid>
           <Grid item>
-            <FormControl sx={{ minWidth: { xs: '30ch', sm: '50ch' } }}>
+            <FormControl sx={{ minWidth: { xs: "30ch", sm: "50ch" } }}>
               {/* <Box sx={{ mx: 'auto' }}>
                   <CircularProgress color="secondary" />
                 </Box> */}
@@ -285,10 +331,10 @@ const EditProfileForm = () => {
                 size="large"
                 disableElevation
                 sx={{
-                  borderRadius: '1rem',
-                  textTransform: 'none',
-                  background: '#7126B5',
-                  py: '15px',
+                  borderRadius: "1rem",
+                  textTransform: "none",
+                  background: "#7126B5",
+                  py: "15px",
                 }}
               >
                 Simpan
@@ -298,7 +344,7 @@ const EditProfileForm = () => {
         </Grid>
       </Box>
     </div>
-  )
-}
+  );
+};
 
-export default EditProfileForm
+export default EditProfileForm;
